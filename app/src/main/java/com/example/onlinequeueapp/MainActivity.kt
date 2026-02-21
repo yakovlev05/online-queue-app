220a169437f03ce71b884f713033685860ee9a6f
yakovlev05 <alexey.yakovlev.05@mail.ru>
2026-02-21 18:53:19 +0500

    add 1 activity

--- app/src/main/java/com/example/onlinequeueapp/MainActivity.kt
+++ app/src/main/java/com/example/onlinequeueapp/MainActivity.kt
@@ -4,44 +4,103 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.onlinequeueapp.ui.theme.OnlineQueueAppTheme
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            OnlineQueueAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            MainMenu()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainMenu() {
+    val nameState = remember { mutableStateOf("") }
+    val questionState = remember { mutableStateOf("") }
+    val isShowNameFieldError = remember { mutableStateOf(false) }
+
+    Column(
+        modifier = Modifier
+            .padding(top = 100.dp)
+            .padding(horizontal = 15.dp),
+        verticalArrangement = Arrangement.spacedBy(15.dp)
+
+    ) {
+        RequiredTextField(
+            state = nameState,
+            isShowErrorState = isShowNameFieldError
+        )
+
+        OutlinedTextField(
+            value = questionState.value,
+            onValueChange = { questionState.value = it },
+            placeholder = { Text(stringResource(R.string.text_field_question_placeholder)) },
+            modifier = Modifier.fillMaxWidth(),
+            shape = RoundedCornerShape(16.dp)
+        )
+
+        Button(
+            onClick = {
+                if (nameState.value.isEmpty()) {
+                    isShowNameFieldError.value = true
+                    return@Button;
+                }
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(56.dp),
+            shape = RoundedCornerShape(16.dp)
+        ) {
+            Text(text = "Отправить")
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Composable
+fun RequiredTextField(state: MutableState<String>, isShowErrorState: MutableState<Boolean>) {
+    OutlinedTextField(
+        value = state.value,
+        onValueChange = {
+            state.value = it
+            isShowErrorState.value = false
+        },
+        placeholder = { Text(stringResource(R.string.text_field_name_placeholder)) },
+        modifier = Modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(16.dp),
+        isError = isShowErrorState.value
+    )
+
+    if (isShowErrorState.value) {
+        Text(
+            stringResource(R.string.button_text),
+            color = Color.Red,
+            modifier = Modifier.padding(start = 16.dp)
+        )
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreview() {
-    OnlineQueueAppTheme {
-        Greeting("Android")
-    }
+    MainMenu()
 }
